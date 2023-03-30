ea00b3270d6d5e652cb404a12203b06173c5cd1a
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-03-30 19:15:19 +0530

    Improvised approach

--- celeb_problem.cpp
+++ celeb_problem.cpp
@@ -55,3 +55,53 @@ int main()
         cout << ob.celebrity(M, n) << endl;
     }
 }
+
+// Above approach takes time of O(n^2)
+// Below approach takes O(n)
+
+//    int celebrity(vector<vector<int> >& M, int n) 
+//     {
+//        int Cel = -1;
+//         stack<int> stack;
+        
+//         for(int x = 0;x<n;x++){
+//             stack.push(x);
+//         }
+        
+//         while(stack.size()>1){
+            
+//             int A = stack.top();
+//             stack.pop();
+//             int B = stack.top();
+//             stack.pop();
+            
+//             if(M[A][B] == 1){
+//             stack.push(B);
+//             }
+//             else{
+//                 stack.push(A);
+//             }
+//         }
+        
+//         Cel = stack.top();
+        
+//         int count0 = 0; 
+//         for(int y = 0;y<n;y++){
+//             if(M[Cel][y] == 0){
+//                 count0++;
+//             }
+            
+//         }
+//         int count1 = 0;
+//         for(int y = 0;y<n;y++){
+//             if(M[y][Cel] == 1){
+//                 count1++;
+//             }
+//         }
+        
+//         if(count0==n&&count1==n-1){
+//             return Cel;
+//         }
+//         return -1;
+        
+//     }
