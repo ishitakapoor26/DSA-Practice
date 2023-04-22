5ba5cb2521373ba71cb22fa0a3b746b24c2e994d
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-04-22 16:07:39 +0530

    DSA

---
+++ recur_increase.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void print_increasing(int n){
+    if(n==0){
+        return;
+    }else{
+        print_increasing(n-1);
+        cout<<n<<endl;
+    }
+}
+
+int main(){
+    print_increasing(5);
+    return 0;
+}
