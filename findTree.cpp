cbca5cdc13464eae247344e40bef9ed45820750d
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-04-27 00:40:27 +0530

    DSA

---
+++ findTree.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node
+{
+    int data;
+    vector<Node *> children;
+};
+
+Node *newNode(int val)
+{
+    Node *node = new Node;
+    node->data = val;
+    return node;
+}
+
+bool find(Node *node, int data)
+{
+    if (node->data == data)
+    {
+        return true;
+    }
+    for (Node *child : node->children)
+    {
+        bool fic = find(child, data);
+        if (fic)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main()
+{
+    Node *root = newNode(10);
+    root->children.push_back(newNode(20));
+    root->children.push_back(newNode(30));
+    root->children.push_back(newNode(40));
+    root->children[0]->children.push_back(newNode(50));
+    root->children[0]->children.push_back(newNode(60));
+    root->children[1]->children.push_back(newNode(70));
+    root->children[1]->children.push_back(newNode(80));
+    root->children[1]->children.push_back(newNode(90));
+    root->children[1]->children[1]->children.push_back(newNode(110));
+    root->children[1]->children[1]->children.push_back(newNode(120));
+    root->children[2]->children.push_back(newNode(100));
+    cout << find(root, 80);
+    return 0;
+}
