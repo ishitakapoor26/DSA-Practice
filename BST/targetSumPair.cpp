1588591f3b88ac1e22887ac41b5e402aeab9c26c
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-07-10 14:30:52 +0530

    Changes in code

--- BST/targetSumPair.cpp
+++ BST/targetSumPair.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <stack>
 #include <algorithm>
 using namespace std;
 
@@ -16,47 +17,163 @@ public:
     }
 };
 
-bool findTree(Node *root, int n)
+// Approach1: SC: height, TC: nlogn
+// bool findTree(Node *root, int n)
+// {
+//     if(root==nullptr){
+//         return false;
+//     }
+//     bool res;
+//     if (root->data == n)
+//     {
+//         return true;
+//     }
+//     if (n < root->data)
+//     {
+//         int res = findTree(root->left, n);
+//         if (res)
+//         {
+//             return true;
+//         }
+//     }
+//     else if (n > root->data)
+//     {
+//         int res = findTree(root->right, n);
+//         if (res)
+//         {
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+
+// void traversalAndPrint(Node* root,Node* node, int tar){
+//     if(node==nullptr){
+//         return;
+//     }
+//     traversalAndPrint(root,node->left,tar);
+//     int comp= tar- node->data;
+//     if(node->data<comp){
+//         if(findTree(root,comp)==true){
+//             cout<<node->data<<" , "<<comp<<endl;
+//         }
+//     }
+//     traversalAndPrint(root,node->right,tar);
+// }
+
+// Approach2: SC & TC: n
+// vector<int> vec;
+
+// void tnf(Node *root)
+// {
+//     if (root == nullptr)
+//     {
+//         return;
+//     }
+//     tnf(root->left);
+//     vec.push_back(root->data);
+//     tnf(root->right);
+// }
+
+// SC & TC: h
+
+class ITPair
 {
-    if(root==nullptr){
-        return false;
-    }
-    bool res;
-    if (root->data == n)
+public:
+    Node *node;
+    int state = 0;
+    ITPair(Node *node, int state)
     {
-        return true;
+        this->node = node;
+        this->state = state;
     }
-    if (n < root->data)
+};
+
+Node *getNextFromNormalInorder(stack<ITPair> st)
+{
+    while (st.size() > 0)
     {
-        int res = findTree(root->left, n);
-        if (res)
+        ITPair top = st.top();
+        if (top.state == 0)
         {
-            return true;
+            if (top.node->left != nullptr)
+            {
+                st.push(ITPair(top.node->left, 0));
+            }
+            top.state++;
+        }
+        else if (top.state == 1)
+        {
+            if (top.node->right != nullptr)
+            {
+                st.push(ITPair(top.node->right, 1));
+            }
+            top.state++;
+            return top.node;
+        }
+        else
+        {
+            st.pop();
         }
     }
-    else if (n > root->data)
-    {
-        int res = findTree(root->right, n);
-        if (res)
-        {
-            return true;
-        }
-    }
-    return false;
+    return nullptr;
 }
 
-void traversalAndPrint(Node* root,Node* node, int tar){
-    if(node==nullptr){
-        return;
-    }
-    traversalAndPrint(root,node->left,tar);
-    int comp= tar- node->data;
-    if(node->data<comp){
-        if(findTree(root,comp)==true){
-            cout<<node->data<<" , "<<comp<<endl;
+Node *getNextFromReverseInorder(stack<ITPair> st)
+{
+    while (st.size() > 0)
+    {
+        ITPair top = st.top();
+        if (top.state == 0)
+        {
+            if (top.node->right != nullptr)
+            {
+                st.push(ITPair(top.node->right, 0));
+            }
+            top.state++;
+        }
+        else if (top.state == 1)
+        {
+            if (top.node->left != nullptr)
+            {
+                st.push(ITPair(top.node->left, 0));
+            }
+            top.state++;
+            return top.node;
+        }
+        else
+        {
+            st.pop();
         }
     }
-    traversalAndPrint(root,node->right,tar);
+    return nullptr;
+}
+
+void bestApproach(Node *root, int tar)
+{
+    stack<ITPair> ls;
+    stack<ITPair> rs;
+    ls.push(ITPair(root, 0));
+    rs.push(ITPair(root, 0));
+    Node *left = getNextFromNormalInorder(ls);
+    Node *right = getNextFromReverseInorder(rs);
+    while (left->data < right->data)
+    {
+        if (left->data + right->data < tar)
+        {
+            left = getNextFromNormalInorder(ls);
+        }
+        else if (left->data + right->data > tar)
+        {
+            right = getNextFromReverseInorder(rs);
+        }
+        else
+        {
+            cout << left->data << " , " << right->data << endl;
+            left = getNextFromNormalInorder(ls);
+            right = getNextFromReverseInorder(rs);
+        }
+    }
 }
 
 int main()
@@ -68,6 +185,23 @@ int main()
     root->left->left = new Node(20);
     root->left->right = new Node(37);
     root->right = new Node(75);
-    traversalAndPrint(root,root,t);
+    // traversalAndPrint(root,root,t);
+    // tnf(root);
+    // int li = 0, ri = vec.size() - 1;
+    // while (li<ri)
+    // {
+    //     int left= vec[li];
+    //     int right= vec[ri];
+    //     if(left+right<t){
+    //         li++;
+    //     }else if(left+right>t){
+    //         ri--;
+    //     }else{
+    //         cout<<left<<" , "<<right<<endl;
+    //         li++;
+    //         ri--;
+    //     }
+    // }
+    bestApproach(root,t);
     return 0;
 }
