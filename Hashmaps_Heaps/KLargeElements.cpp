1685875315423b9f6e1462e6cab04ad0b896869e
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-07-11 16:23:23 +0530

    K large elements in array heaps

---
+++ Hashmaps_Heaps/KLargeElements.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+    int *arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (i < k)
+        {
+            pq.push(arr[i]);
+        }
+        else
+        {
+            if (pq.top() < arr[i])
+            {
+                pq.pop();
+                pq.push(arr[i]);
+            }
+        }
+    }
+    for (int i = 0; i < k; i++)
+    {
+        cout << pq.top() << " , ";
+        pq.pop();
+    }
+    return 0;
+}
