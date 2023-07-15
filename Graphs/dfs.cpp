4ec966da24de1ba0f6c04cb94ee482d10dba73bd
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-07-15 09:05:00 +0530

    DFS complete

--- Graphs/dfs.cpp
+++ Graphs/dfs.cpp
@@ -1,13 +1,34 @@
-#include<iostream>
-#include<vector>
-#include<queue>
+#include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
-vector<int> dfs(int v, vector<int> adj[]){
-    
-}
+class Solution
+{
+private:
+    void dfsHelper(int node, vector<int> adj[], int vis[], vector<int> &ls)
+    {
+        vis[node]=1;
+        ls.push_back(node);
+        for(auto it: adj[node]){
+            if(!vis[it]){
+                dfsHelper(it, adj,vis, ls);
+            }
+        }
+    }
 
-int main(){
+public:
+    vector<int> dfs(int v, vector<int> adj[])
+    {
+        int* vis= new int[v];
+        int start =0;
+        vector<int> ls;
+        dfsHelper(start, adj, vis, ls);
+        return ls;
+    }
+};
+
+int main()
+{
     return 0;
 }
-// will continue from tomorrow.
