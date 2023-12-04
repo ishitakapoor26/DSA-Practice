107713ab935ce666cf25a601372e33a453e40280
Ishita Kapoor <ishitakapoor12612@gmail.com>
2023-12-04 19:31:43 +0530

    Time: 271 ms (90.29%), Space: 131 MB (6.66%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        if(n==0 || n==1) return 0;
+        vector<int> prime(n+1,1);
+    for(int i=2;i*i<n;i++){
+        if(prime[i]){
+            for(int j=i*i;j<n;j+=i){
+                prime[j]=0;
+            }
+        }
+    }
+    int ans=0;
+    for(int i=2;i<n;i++){
+        if(prime[i]){
+        ans++;
+        } 
+    }
+    return ans;
+    }
+};
